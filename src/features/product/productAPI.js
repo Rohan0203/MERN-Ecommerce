5e47699426a22205355ad9025df5297146d6f21c
rohan.bakhru21@gmail.com <rohan.bakhru21@gmail.com>
2024-05-18 12:31:34 +0530

    Added code for Ecommerce

--- src/features/product/productAPI.js
+++ src/features/product/productAPI.js
@@ -1,6 +1,7 @@
-export function fetchCount(amount = 1) {
+export function fetchAllProduct() {
   return new Promise(async (resolve) =>{
-      const response = await fetch('https://localhost:8080');
+    
+      const response = await fetch('https://localhost:8080/products');
       const data = await response.json();
       resolve({data})
     }
